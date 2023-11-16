13cce7c4d4b8c0f2a562629af3d6e09b91d835fc
Thevananthan Thevarasa <thevananthan.thevarasa@rinstrum.com>
2023-11-16 16:13:14 -0330

    added the app init and main scripts

---
+++ config.py
@@ -0,0 +1,3 @@
+class Config:
+    SECRET_KEY = 'data_server_hubert'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///users.db'
---
+++ main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+"""Web App for Viewing Data Viewer"""
+__author__ = "Thevananthan Thevarasa"
+__copyright__ = "Copyright 2023, Hensley Hubert Mariathas"
+__email__ = "thevananthan.thevarasa@gmail.com"
+
+
+from app import create_app, set_dirs
+import os
+
+# Get the current working directory
+current_dir = os.getcwd()
+
+# Define the template and static directories by appending to the current directory
+static_dir = os.path.join(current_dir, 'static')
+template_dir = os.path.join(current_dir, 'templates')
+
+# Create the Flask application
+set_dirs(static_dir, template_dir)
+app = create_app()  # creating an instance of your application
+
+# Run the App
+if __name__ == '__main__':
+    app.run(debug=True)
